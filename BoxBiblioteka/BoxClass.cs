0b2b903df23c7fedc0735df458a294f6da563a2b
lucjan25 <lucjan25.t@gmail.com>
2022-06-05 20:25:03 +0200

    Add project files.

---
+++ BoxBiblioteka/BoxClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxBiblioteka
+{
+    public interface IBox
+    {
+        public void Add(IBox item);
+        public double Cena();
+        public double GetCenaJednostkowa();
+    }
+    public class Box : IBox
+    {
+        List<IBox> content = new List<IBox>();
+        private double cena;
+        public Box(double cena)
+        {
+            this.cena = cena;
+        }
+        public void Add(IBox item)
+        {
+            content.Add(item);
+        }
+
+        public double Cena()
+        {
+            double Cena = this.GetCenaJednostkowa();
+            foreach (IBox item in content)
+            {
+                Cena += item.Cena();
+            }
+            return Cena;
+        }
+
+        public double GetCenaJednostkowa()
+        {
+            return this.cena;
+        }
+    }
+    public class Item : IBox
+    {
+        private double cena;
+        public Item(double cena)
+        {
+            this.cena = cena;
+        }
+        public void Add(IBox item)
+        {
+            return;
+        }
+
+        public double Cena()
+        {
+            return GetCenaJednostkowa();
+        }
+
+        public double GetCenaJednostkowa()
+        {
+            return this.cena;
+        }
+    }
+    public class Container : IBox
+    {
+        List<Box> order = new List<Box>();
+        public void Add(IBox item)
+        {
+            if (item.GetType() == typeof(Item)) return;
+            order.Add((Box)item);
+        }
+
+        public double Cena()
+        {
+            double Cena = 0;
+            foreach (IBox item in order)
+            {
+                Cena += item.Cena();
+            }
+            return Cena;
+        }
+
+        public double GetCenaJednostkowa()
+        {
+            return 0;
+        }
+    }
+}
---
+++ BoxTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BoxBiblioteka;
+
+namespace BoxTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Container sektor1 = new Container();
+            Container sektor2 = new Container();
+
+            Box box1 = new(500.50);
+            Box box2 = new(22.2);
+            Box box3 = new(1.1);
+            Box box4 = new(0.7);
+
+            Item item1 = new(40.1);
+            Item item2 = new(1.7);
+            Item item3 = new(0.6);
+            Item item4 = new(943.0);
+            Item item5 = new(14.5);
+            Item item6 = new(.81);
+
+            box1.Add(box2);
+            box2.Add(item1);
+            sektor1.Add(box1);
+            sektor2.Add(box3);
+            box3.Add(box4);
+            box3.Add(item2);
+            box1.Add(item3);
+            box4.Add(item4);
+            box2.Add(item5);
+            box4.Add(item6);
+
+            Console.WriteLine("Cena kontenera 1:" + sektor1.Cena());
+            Console.WriteLine("Cena kontenera 2:" + sektor2.Cena());
+        }
+    }
+}
